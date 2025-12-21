00b74c5b82b95d91a1fd501a2f5400dcf54ba65d
BumTiger <jbpark03@gmail.com>
2025-12-21 14:38:25 +0900

    종속성 3 종 추가 후 - 초기 상태

---
+++ src/main/java/com/bumsoap/tracker_more/TrackerMoreApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.tracker_more;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerMoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumsoap/tracker_more/TrackerMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumsoap.tracker_more;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerMoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
